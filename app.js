7e09a9171841b3b8a8a87e6a51e4734ce90634aa
Colin But <colin.but@outlook.com>
2019-10-12 23:49:19 +0100

    tidy app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+let port = 3000
+
 app.get('/', (req, res) => res.send("Hello World!"))
 
-app.listen(3000, () => console.log("App listening on port 3000"))
+app.listen(port, () => console.log(`App listening on port ${port}`))
